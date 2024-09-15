02d65f86bb769e2aa3e2236731b3f7f379bfd450
uoser <uoser@OMARMAGDY>
2024-09-15 21:10:51 +0300

    department

---
+++ Controllers/DeoartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FristApp.Controllers
+{
+    public class DeoartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
